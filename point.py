f3df66bc8bf129554e1839143a93b53c8ec892c6
Tani N-K <tani@taniwritescode.dev>
2020-09-15 14:50:54 -0400

    Sorry, still not exactly atomic
    A litttllleee more atomic, but really still not atomic

--- point.py
+++ point.py
@@ -9,10 +9,11 @@ class Point:
         self.x = x
         self.y = y
 
+    def __repr__(self):
+        return "({}, {})".format(float(self.x), float(self.y))
 
     def dist_to(self, p):
         return sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
-
 
     def abs(self):
         return sqrt(self.x**2 + self.y**2)
@@ -34,8 +35,9 @@ class Rectangle:
         return abs((self.width * 2) + (self.length * 2))
 
     def inside(self, point):
-        return True if (self.p1.x <= point.x <= self.p2.x) \
-                   and (self.p2.y <= point.y <= self.p2.y)
+        if self.p1.x <= point.x <= self.p2.x:
+            if self.p1.y <= point.y <= self.p2.y:
+                return True
         return False
 
 class Circle:
@@ -44,14 +46,13 @@ class Circle:
         self.radius = radius
 
     def area(self):
-        return pi * self.radius ** 2
+        return float(pi * self.radius ** 2)
 
-    def perimiter(self):
-        return self.radius * 2 * pi
+    def perimeter(self):
+        return float(self.radius * 2 * pi)
 
     def inside(self, point):
-        if abs(point.x - self.center.x) <= self.radius \
-       and abs(point.y - self.center.y) <= self.radius:
+        if point.dist_to(self.center) <= self.radius:
             return True
         return False
 
@@ -59,12 +60,19 @@ def main():
     p1 = Point(3.0, 4.0)
     print("The point ({}, {}) is at a distance of {} from the origin.".format(p1.x, p1.y, p1.abs()))
 
-    p2 = Point(-1.0, 6.5)
+    p2 = Point(-1.0, 40)
     print("It is a distance {:.5} away from the point ({}, {}).".format(p1.dist_to(p2), p2.x, p2.y))
 
-    a = Rectangle(Point(5, 5), Point(0, 0))
-    print(a.perimeter())
-    
+    r = Rectangle(Point(5, 5), Point(0, 0))
+    print("A rectangle of the corners {} and {} has a parimeter of {} and an area of {}"
+          .format(r.c1, r.c2, r.perimeter(), r.area()))
+
+    c = Circle(Point(0,0), 5)
+    print("A circle of originating at {} with a radius of {} has a perimeter of {:.5} and an area of {:.5}"
+          .format(c.center, c.radius, c.perimeter(), c.area()))
+
+    print("The point {} is inside the circle: {}".format(p1, c.inside(p1)))
+    print("The point {} is inside the circle: {}".format(p2, c.inside(p2)))
 
 if __name__ == "__main__":
     main()
